ecd8464da8df53245300d69de3f57c551c4dbbbd
janakiramkusu <160325078+janakiramkusu@users.noreply.github.com>
2025-05-19 18:30:37 +0530

    Update Logos.jsx

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -18,24 +18,22 @@ export default function Logos() {
       </h2>
 
       <div className="relative w-full overflow-hidden">
+        
         <div className="pointer-events-none absolute left-0 top-0 h-full w-[30vw] bg-gradient-to-r from-black to-transparent z-10 xl:hidden" />
         <div className="pointer-events-none absolute right-0 top-0 h-full w-[30vw] bg-gradient-to-l from-black to-transparent z-10 xl:hidden" />
-        <div className="flex animate-scroll whitespace-nowrap gap-12 px-8">
-          {Array(4)
-            .fill(logos)
-            .flat()
-            .map((logo, index) => (
-              <figure
-                key={index}
-                className="flex min-w-[160px] h-20 items-center justify-center px-4 py-3"
-              >
-                <img
-                  src={`/logos/${logo}.svg`}
-                  alt={logo}
-                  className="w-32 lg:w-40 object-contain"
-                />
-              </figure>
-            ))}
+        <div className="flex animate-scroll whitespace-nowrap gap-12 px-8 min-w-fit">
+          {[...logos, ...logos].map((logo, index) => (
+            <figure
+              key={index}
+              className="flex min-w-[160px] h-20 items-center justify-center px-4 py-3 shrink-0"
+            >
+              <img
+                src={`/logos/${logo}.svg`}
+                alt={logo}
+                className="w-32 object-contain"
+              />
+            </figure>
+          ))}
         </div>
       </div>
     </section>
